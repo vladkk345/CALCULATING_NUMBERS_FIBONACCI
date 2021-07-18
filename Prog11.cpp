baf34b08f4bb20e85aa3176e40fd2c532ce198cb
Владик Кононович <55635774+vladkk345@users.noreply.github.com>
2021-07-18 20:25:42 +0300

    Add files via upload

---
+++ Prog11.cpp
@@ -0,0 +1,32 @@
+﻿// Prog11.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+
+int Fibonacci(int n);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int x;
+	x = Fibonacci(5);
+	printf("Результат %d\n", x);
+
+	return 0;
+}
+
+int Fibonacci(int n)
+{
+	int res = 1, temp = 0;
+
+	for (int i = 1; i < n; i++)
+	{	
+		res = res + temp;
+		temp = res - temp;
+	}
+	return res;
+}
